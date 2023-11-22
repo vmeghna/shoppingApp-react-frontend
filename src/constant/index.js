faaa6dbac2d6027f4b85459593130f68f66beb3d
vmeghna <shrimeghna97@gmail.com>
2023-11-22 08:31:15 +0530

    basic setup done

---
+++ src/constant/index.js
@@ -0,0 +1,60 @@
+const categories = [
+  { id: 1, title: "Marv", imageUrl: "Rockingham" },
+  { id: 2, title: "Shirley", imageUrl: "Taunton" },
+  { id: 3, title: "Lavena", imageUrl: "Tarbin" },
+  { id: 4, title: "Janith", imageUrl: "Christer" },
+  { id: 5, title: "Townsend", imageUrl: "Spadari" },
+];
+const SHOP_DATA = [
+  {
+    title: "women",
+    items: [
+      { id: 1, name: "Doloritas", imageUrl: "Keppel", price: "Boylan" },
+      { id: 2, name: "Moselle", imageUrl: "McFarlan", price: "Edinboro" },
+      { id: 3, name: "Deanne", imageUrl: "Cambell", price: "Felmingham" },
+      { id: 4, name: "Dewey", imageUrl: "Bendan", price: "Mapplethorpe" },
+      { id: 5, name: "Yanaton", imageUrl: "Kuhnke", price: "Backes" },
+    ],
+  },
+  {
+    title: "men",
+    items: [
+      { id: 1, name: "Doloritas", imageUrl: "Keppel", price: "Boylan" },
+      { id: 2, name: "Moselle", imageUrl: "McFarlan", price: "Edinboro" },
+      { id: 3, name: "Deanne", imageUrl: "Cambell", price: "Felmingham" },
+      { id: 4, name: "Dewey", imageUrl: "Bendan", price: "Mapplethorpe" },
+      { id: 5, name: "Yanaton", imageUrl: "Kuhnke", price: "Backes" },
+    ],
+  },
+  {
+    title: "sneakers",
+    items: [
+      { id: 1, name: "Doloritas", imageUrl: "Keppel", price: "Boylan" },
+      { id: 2, name: "Moselle", imageUrl: "McFarlan", price: "Edinboro" },
+      { id: 3, name: "Deanne", imageUrl: "Cambell", price: "Felmingham" },
+      { id: 4, name: "Dewey", imageUrl: "Bendan", price: "Mapplethorpe" },
+      { id: 5, name: "Yanaton", imageUrl: "Kuhnke", price: "Backes" },
+    ],
+  },
+  {
+    title: "Accessories",
+    items: [
+      { id: 1, name: "Doloritas", imageUrl: "Keppel", price: "Boylan" },
+      { id: 2, name: "Moselle", imageUrl: "McFarlan", price: "Edinboro" },
+      { id: 3, name: "Deanne", imageUrl: "Cambell", price: "Felmingham" },
+      { id: 4, name: "Dewey", imageUrl: "Bendan", price: "Mapplethorpe" },
+      { id: 5, name: "Yanaton", imageUrl: "Kuhnke", price: "Backes" },
+    ],
+  },
+  {
+    title: "Skin care",
+    items: [
+      { id: 1, name: "Doloritas", imageUrl: "Keppel", price: "Boylan" },
+      { id: 2, name: "Moselle", imageUrl: "McFarlan", price: "Edinboro" },
+      { id: 3, name: "Deanne", imageUrl: "Cambell", price: "Felmingham" },
+      { id: 4, name: "Dewey", imageUrl: "Bendan", price: "Mapplethorpe" },
+      { id: 5, name: "Yanaton", imageUrl: "Kuhnke", price: "Backes" },
+    ],
+  },
+];
+export { categories, SHOP_DATA };
